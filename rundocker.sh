45167974f9ef9ab5c62d8061a63562e19fe356b7
lava_frai <lavafrai@yandex.ru>
2024-02-24 15:46:04 +0300

    Docker fix 3

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,3 @@
 docker stop maiserver
 docker rm maiserver
-docker run -it -d -v $(pwd):/opt/app --restart=always -p 8085:80 --name=maiserver $(docker build -q .)
+docker run -it -d --restart=always -p 8085:80 --name=maiserver $(docker build -q .)
